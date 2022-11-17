dfe8f47d3c739934b5dac85e2624dc4efcc5b8a9
3seoksw <3suksw@gmail.com>
2022-11-17 20:19:39 +0900

    vimtex.lua live view changed

--- lua/KWS/plugins/VimTex.lua
+++ lua/KWS/plugins/VimTex.lua
@@ -3,5 +3,5 @@ vim.g.tex_flavor = "latex"
 vim.g.vimtex_view_method = "skim"
 vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
 vim.g.vimtex_quickfix_mod = 0
-vim.cmd[["set conceallevel = 1"]]
+vim.cmd[["set conceallevel = 2"]]
 vim.g.tex_conceal = "abdmg"
